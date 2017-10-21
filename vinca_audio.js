3722b065bbca2ed701937b5c57e5cf2ce0d9ec68
valedeia <32706537+valedeia@users.noreply.github.com>
2017-10-22 01:16:44 +0200

    Add files via upload

---
+++ vinca_audio.js
@@ -0,0 +1,411 @@
+var sounds = {
+	"type": "FeatureCollection",
+	"features": [
+	{
+		"type": "Feature",
+		"properties": {
+			"name": "Asti",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/asti.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [8.2,44.9]
+		}
+	},
+
+
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Milano",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/milano.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [9.18333,45.466667]
+		}
+	},
+
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Genova",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/genova.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [8.932778,44.411111]
+		}
+	},
+
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Verona",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/verona2.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [10.983333,45.433333]
+		}
+	},
+
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Bologna",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/bologna.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [11.32141,44.51149]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Campobasso",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/campobasso.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [14.66272,41.56025] 
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Firenze",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/firenze.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [11.25581,43.76956]
+		}
+	},
+
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Gorizia",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/gorizia.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [13.62018,45.94018]	
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Lecce",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/lecce.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [18.17502,40.35152]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Napoli",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/napoli.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [14.26812,40.85177] 
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Nuoro",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/nuoro.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [9.32644,40.32024]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Oristano",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/oristano.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [8.58839,39.90618]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Palermo",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/palermo.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [13.36127,38.11569]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Perugia",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/perugia.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [12.39083,43.11072]
+		}
+	},
+
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Pistoia",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/pistoia.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [10.90786,43.93035]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Reggio Calabria",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/reggiocalabria.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [15.64729,38.11130]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Roma",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/roma.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [12.49637,41.90278]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Siracusa",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/siracusa.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [15.28659,37.07547]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Taranto",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/taranto.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [17.24703,40.46436]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Trapani",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/trapani.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [12.53720,38.01762]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Trieste",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/trieste.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [13.77682,45.64953]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Bari",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/bari.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [16.87187,41.11714]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Latina",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/latina.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [12.90360,41.46757]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Padova",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/padova.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [11.87676,45.40643]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Carloforte (CA)",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/carloforte.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [8.30298,39.14013]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Gizzeria (CZ)",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/gizzeria.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [16.20913,38.98157]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Viterbo",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/viterbo.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [12.10767,42.42068]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Umbertide (PG)",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/umbertide.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [12.32787,43.30557]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Lecco",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/lecco.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p><p><a download href='http://www.lfsag.unito.it/ark/06z7bwka1.TextGrid'>prova</a></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [9.39767,45.85657]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Torino",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/torino.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [7.68686,45.07031]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Frosinone",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/frosinone.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [13.34718,41.64311]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Pisa",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/pisa.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [10.40169,43.72284]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Canicattì",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/canicatti.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [13.85066,37.35817]
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Ripa Teatina (CH)",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/ripa_teatina.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [14.23101,42.35627] 
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Brescia",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/brescia.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [10.21180,45.54155] 
+		}
+	},
+{
+		"type": "Feature",
+		"properties": {
+			"name": "Pianiga (VE)",
+			"html": "<p><audio width='300' height='32' src='ark/vinca/pianiga.mp3' controls='controls'><br />Your browser does not support the audio element.<br /></audio></p>"
+		},
+		"geometry": {
+			"type": "Point",
+			"coordinates": [12.07772,45.43860] 
+		}
+	},
+
+]
+
+
+}
