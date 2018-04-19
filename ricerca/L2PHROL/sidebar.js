42f2165a261b9a513d6a51ccfb5a2c309a4ad44c
Olivier Friard <olivier.friard@unito.it>
2018-04-19 19:01:21 +0200

    some work

---
+++ ricerca/L2PHROL/sidebar.js
@@ -0,0 +1,14 @@
+document.write('<div id="sidebar2">');
+document.write('<h3>News</h3>');
+document.write('<ul class="linkedList">');
+document.write('<li class="first">7th October 2017: <i>The programme is finally available. <a href="./programme.html">Check it here!</a></i>');
+document.write('<li>23rd September 2017: <i>abstract reviews have been sent to authors. The programme will follow soon (early October).</i>');
+document.write('<li>9th September 2017: <i>abstract submission is now closed and the reviewing process has started.</i>');
+document.write('<li>29th August 2017: <i>the deadline for submitting abstracts has been <a href="./dates.html">extended to 8 September 2017</a></i></li>')
+document.write('<li>8th July 2017: <i>we now have <a href="./programme.html">confirmed guest speakers</a></i></li>')
+document.write('<li>7th July 2017: <i>we have started receiving abstracts!</i></li>')
+document.write('<li>15 June 2017: <i>the abstract submission procedure is now open via EasyChair. <a href="https://easychair.org/conferences/?conf=l2phrol">Click here to submit your abstract!</a></i></li>');
+document.write('<li>26 May 2017: <i>the <a href="./call.html">call for papers</a> has been published!</a></i></li>');
+document.write('<li class="last">19 April 2017: <i>the website has been published, the call for papers will follow soon, stay tuned!</i></li>');
+document.write('</ul>');
+document.write('</div>');
